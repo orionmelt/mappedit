093ec24d4482ff5eaada5e22e21b017fe0c8e47d
Raj N <orionmelt@gmail.com>
2022-11-13 01:18:46 -0800

    Add map styles

--- src/Map.js
+++ src/Map.js
@@ -7,6 +7,231 @@ import "./Map.css";
 
 const DEFAULT_CENTER = [39.50024, -98.350891];
 const DEFAULT_ZOOM = 4;
+const MAP_OPTIONS = {
+  mapTypeControl: false,
+  fullscreenControl: false,
+  styles: [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#242f3e"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#746855"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#242f3e"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#d59563"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#d59563"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.business",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#263c3f"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#6b9a76"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#38414e"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#212a37"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9ca5b3"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#746855"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#1f2835"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#f3d19c"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#2f3948"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#d59563"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#17263c"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#515c6d"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#17263c"
+        }
+      ]
+    }
+  ]
+};
 
 class Map extends React.Component {
   constructor(props) {
@@ -21,7 +246,7 @@ class Map extends React.Component {
 
   getRadius = (place) => {
     const totalSubscribers = place.subreddits.map(s => s.subscribers).reduce((a, b) => a+b, 0);
-    return Math.log(totalSubscribers);
+    return (Math.log(totalSubscribers)/Math.LN10)*10;
   }
 
   unselectPlace = () => {
@@ -46,7 +271,7 @@ class Map extends React.Component {
           opacity: 0.8,
           stroked: true,
           filled: true,
-          radiusScale: 4096,
+          radiusScale: 1000,
           radiusMinPixels: 1,
           radiusMaxPixels: 16,
           lineWidthMinPixels: 1,
@@ -59,7 +284,7 @@ class Map extends React.Component {
         })
       ]
     });
-    deckOverlay.setMap(map);  
+    deckOverlay.setMap(map);
   }
 
   render() {
@@ -72,6 +297,7 @@ class Map extends React.Component {
           center={mapCenter}
           zoom={DEFAULT_ZOOM}
           yesIWantToUseGoogleMapApiInternals
+          options={MAP_OPTIONS}
           onGoogleApiLoaded={({map, maps}) => this.onMapLoaded(map, maps)}
         >
           {selectedPlace &&
